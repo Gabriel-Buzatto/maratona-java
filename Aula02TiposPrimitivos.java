35319189a60323fbbe19419804ec1c7ab52baab8
Gabriel Buzatto <141946645+Gabriel-Buzatto@users.noreply.github.com>
2025-03-26 15:30:14 -0300

    Add files via upload

---
+++ Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 100000;
+        double salaryDouble = 2000.0;
+        float salaryFloat = 2500.0F;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String name = "Gabriel";
+
+        System.out.println("A idade é "+ age + "anos");
+
+        System.out.println("Oi, meu nome é " + name);
+    }
+}
